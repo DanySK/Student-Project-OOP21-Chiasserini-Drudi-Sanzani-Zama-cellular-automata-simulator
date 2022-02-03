6beac1c494eb42651b94729a228216ae7b1a5df3
Filippo Sanzani <filippo.sanzani@studio.unibo.it>
2022-02-03 10:58:36 +0100

    Add ResultTest class.

---
+++ app/src/test/java/casim/utils/ResultTest.java
@@ -0,0 +1,112 @@
+package casim.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ResultTest {
+    @Test
+    public void testEquals() {
+        fail();
+    }
+
+    @Test
+    public void testError() {
+        fail();
+    }
+
+    @Test
+    public void testFlatMap() {
+        fail();
+    }
+
+    @Test
+    public void testFlatMap2() {
+        fail();
+    }
+
+    @Test
+    public void testGetError() {
+        fail();
+    }
+
+    @Test
+    public void testGetValue() {
+        fail();
+    }
+
+    @Test
+    public void testHashCode() {
+        fail();
+    }
+
+    @Test
+    public void testIsError() {
+        fail();
+    }
+
+    @Test
+    public void testIsPresent() {
+        fail();
+    }
+
+    @Test
+    public void testMap() {
+        fail();
+    }
+
+    @Test
+    public void testMapError() {
+        fail();
+    }
+
+    @Test
+    public void testOf() {
+        fail();
+    }
+
+    @Test
+    public void testOfEmpty() {
+        fail();
+    }
+
+    @Test
+    public void testOrElse() {
+        fail();
+    }
+
+    @Test
+    public void testRequire() {
+        fail();
+    }
+
+    @Test
+    public void testRequire2() {
+        fail();
+    }
+
+    @Test
+    public void testRequire3() {
+        fail();
+    }
+
+    @Test
+    public void testStream() {
+        fail();
+    }
+
+    @Test
+    public void testToEmpty() {
+        fail();
+    }
+
+    @Test
+    public void testToOptional() {
+        fail();
+    }
+
+    @Test
+    public void testToString() {
+        fail();
+    }
+}
