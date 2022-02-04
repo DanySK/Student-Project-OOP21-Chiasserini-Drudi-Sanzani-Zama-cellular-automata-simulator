b16a2710cb1cc930a890775a4c57858caeb5fd0f
Filippo Sanzani <filippo.sanzani@studio.unibo.it>
2022-02-04 12:11:44 +0100

    Add Coordinate type and operations between them.

---
+++ app/src/main/java/casim/utils/coordinate/Coordinates.java
@@ -0,0 +1,23 @@
+package casim.utils.coordinate;
+
+/**
+ * Utility class that defines methods for the Coordinate type.
+ */
+public final class Coordinates {
+    /**
+     * Sum two {@link Coordinate} objects returning a new {@link Coordinate}.
+     * 
+     * The sum operation generates a {@link Coordinate} that has:
+     *  - X equals to the sum of the X values of a and b
+     *  - Y equals to the sum of the Y values of a and b
+     * 
+     * @param <T> type of the {@link Coordinate} involved.
+     * @param a first element of the sum.
+     * @param b second element of the sum.
+     * @return a new {@link Coordinate} object representing the sum of a and b.
+     */
+    public static <T extends Number> Coordinate<T> sum(Coordinate<T> a, Coordinate<T> b) {
+        //TODO
+        return null;
+    }
+}
