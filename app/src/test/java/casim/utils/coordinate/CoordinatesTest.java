3d1a812d2c9316013556617e78e20eea496561e5
Filippo Sanzani <filippo.sanzani@studio.unibo.it>
2022-02-05 12:03:29 +0100

    Test classes for Coordinates.

---
+++ app/src/test/java/casim/utils/coordinate/CoordinatesTest.java
@@ -0,0 +1,43 @@
+package casim.utils.coordinate;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for {@link Coordinates}.
+ */
+class CoordinatesTest {
+
+    /**
+     * Test for {@link Coordinates#getX()} method. 
+     */
+    @Test
+    void testGetX() {
+        fail();
+    }
+
+    /**
+     * Test for {@link Coordinates#getY()} method.
+     */
+    @Test
+    void testGetY() {
+        fail();
+    }
+
+    /**
+     * Test for {@link Coordinates#setX(Number)} method.
+     */
+    @Test
+    void testSetX() {
+        fail();
+    }
+
+    /**
+     * Test for {@link Coordinates#setY(Number)} method.
+     */
+    @Test
+    void testSetY() {
+        fail();
+    }
+}
---
+++ app/src/test/java/casim/utils/coordinate/CoordinatesUtilTest.java
@@ -0,0 +1,19 @@
+package casim.utils.coordinate;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for {@link CoordinatesUtil}.
+ */
+class CoordinatesUtilTest {
+
+    /**
+     * Test for {@link CoordinatesUtil#sum(Coordinates, Coordinates)} method.
+     */
+    @Test
+    void testSum() {
+        fail();
+    }
+}
