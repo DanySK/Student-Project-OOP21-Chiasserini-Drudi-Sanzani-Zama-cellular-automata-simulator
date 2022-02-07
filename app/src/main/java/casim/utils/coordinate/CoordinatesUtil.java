ee5d8f6438c0ebd048e87c7de3e0fea522961ff6
Simone Zama <simone.zama@studio.unibo.it>
2022-02-07 16:28:00 +0100

    Implement CoordinatesUtil.of and CoordinatesUtil.isValid methods

--- app/src/main/java/casim/utils/coordinate/CoordinatesUtil.java
+++ app/src/main/java/casim/utils/coordinate/CoordinatesUtil.java
@@ -22,7 +22,37 @@ public final class CoordinatesUtil {
      * @return a new {@link Coordinates} object representing the sum of a and b.
      */
     public static <T extends Number> Coordinates<T> sum(final Coordinates<T> a, final Coordinates<T> b) {
-        //TODO
         return null;
+    }
+
+    /**
+     * Create a new {@link Coordinates} object from the values given as arguments.
+     * 
+     * @param <T> type of the {@link Coordinates} involved.
+     * @param x value to be used as coordinate x.
+     * @param y value to be used as coordinate y.
+     * @return a {@link Coordinates} with the x and y values given as arguments.
+     */
+    public static <T extends Number> Coordinates<T> of(final T x, final T y) {
+    	return new CoordinatesImpl<T>(x, y);
+    }
+
+    /**
+     * Checks if the {@link Coordinates} given as argument is inside the rectangle formed by the {@link Coordinates} topLeft and bottomRight.
+     * The check is inclusive for the topLeft {@link Coordinates} but not for bottomRight.
+     * 
+     * @param <T> type of the {@link Coordinates} involved.
+     * @param coord the {@link Coordinates} to be checked against topLeft and bottomRight.
+     * @param topLeft the {@link Coordinates} representing the point at the top left of the rectangle that coord has to be inside of.
+     * @param bottomRight the {@link Coordinates} representing the point at the bottom right of the rectangle that coord has to be inside of.
+     * @return True if coord is inside the rectangle, false otherwise.
+     */
+    public static <T extends Number> boolean  isValid(final Coordinates<T> coord, final Coordinates<T> topLeft, final Coordinates<T> bottomRight) {
+    	final var coordD = CoordinatesUtil.of(coord.getX().doubleValue(), coord.getY().doubleValue());
+    	final var topLeftD = CoordinatesUtil.of(topLeft.getX().doubleValue(), topLeft.getY().doubleValue());
+    	final var bottomRightD = CoordinatesUtil.of(bottomRight.getX().doubleValue(), bottomRight.getY().doubleValue());
+    	
+    	return coordD.getX() >= topLeftD.getX() && coordD.getY() <= topLeftD.getY() 
+				&& coordD.getX() < bottomRightD.getX() && coordD.getY() > bottomRightD.getY();
     }
 }
