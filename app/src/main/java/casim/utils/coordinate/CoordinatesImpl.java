d95d233cf8a0d2d1861c9c486f628f2479014973
Simone Zama <simone.zama@studio.unibo.it>
2022-02-07 11:13:52 +0100

    Create CoordinatesImpl

---
+++ app/src/main/java/casim/utils/coordinate/CoordinatesImpl.java
@@ -0,0 +1,29 @@
+package casim.utils.coordinate;
+
+public class CoordinatesImpl<T extends Number> implements Coordinates<T> {
+
+	@Override
+	public void setX(T value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setY(T value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T getX() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getY() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
