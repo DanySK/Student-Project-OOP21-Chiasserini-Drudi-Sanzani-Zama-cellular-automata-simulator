c9a78cee4a6f8e8e069f6117607d53cbdb666264
Simone Zama <simone.zama@studio.unibo.it>
2022-02-08 15:23:53 +0100

    Fix magic number warnings

--- app/src/test/java/casim/utils/coordinate/CoordinatesUtilTest.java
+++ app/src/test/java/casim/utils/coordinate/CoordinatesUtilTest.java
@@ -10,6 +10,21 @@ import org.junit.jupiter.api.Test;
  */
 class CoordinatesUtilTest {
 
+    static final Coordinates<Integer> INTCOORD01 = CoordinatesUtil.of(10, 5);
+    static final Coordinates<Integer> INTCOORD02 = CoordinatesUtil.of(15, 10);
+    static final Coordinates<Integer> INTCOORD03 = CoordinatesUtil.of(15, 5);
+    static final Coordinates<Integer> INTCOORD04 = CoordinatesUtil.of(15, 0);
+    static final Coordinates<Float> FLOATCOORD01 = CoordinatesUtil.of((float) 10.5, (float) 4.5);
+    static final Coordinates<Float> FLOATCOORD02 = CoordinatesUtil.of((float) 15.6, (float) 6.5);
+    static final Coordinates<Float> FLOATCOORD03 = CoordinatesUtil.of((float) 9.0, (float) 7.5);
+    static final Coordinates<Float> FLOATCOORD04 = CoordinatesUtil.of((float) 7.6, (float) 6.5);
+    static final Coordinates<Double> DOUBLECOORD01 = CoordinatesUtil.of(5.5, 10.5);
+    static final Coordinates<Double> DOUBLECOORD02 = CoordinatesUtil.of(7.4, 12.6);
+    static final Coordinates<Short> SHORTCOORD01 = CoordinatesUtil.of((short) 5, (short) 10);
+    static final Coordinates<Short> SHORTCOORD02 = CoordinatesUtil.of((short) 7, (short) 17);
+    static final Coordinates<Long> LONGCOORD01 = CoordinatesUtil.of((long) 20, (long) 10);
+    static final Coordinates<Long> LONGCOORD02 = CoordinatesUtil.of((long) 25, (long) 25);
+
     /**
      * Test for {@link CoordinatesUtil#sum(Coordinates, Coordinates)} method.
      */
@@ -28,14 +43,13 @@ class CoordinatesUtilTest {
         final var coordD = CoordinatesUtil.of(5.5, 10.5);
         final var coordS = CoordinatesUtil.of((short) 5, (short) 10);
         final var coordL = CoordinatesUtil.of((long) 20, (long) 10);
-        Assert.assertTrue(CoordinatesUtil.isValid(coordI, CoordinatesUtil.of(10, 5), CoordinatesUtil.of(15, 10)));
-        Assert.assertFalse(CoordinatesUtil.isValid(coordI, CoordinatesUtil.of(15, 5), CoordinatesUtil.of(15, 0)));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordF, CoordinatesUtil.of((float) 10.5, (float) 4.5), CoordinatesUtil.of((float) 15.6, (float) 6.5)));
-        Assert.assertFalse(CoordinatesUtil.isValid(coordF, CoordinatesUtil.of((float) 9.0, (float) 7.5), CoordinatesUtil.of((float) 7.6, (float) 6.5)));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordD, CoordinatesUtil.of(5.5, 10.5), CoordinatesUtil.of(7.4, 12.6)));
-        Assert.assertFalse(CoordinatesUtil.isValid(coordD, CoordinatesUtil.of(0.0, 20.9), CoordinatesUtil.of(5.5, 10.5)));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordS, CoordinatesUtil.of((short) 5, (short) 10), CoordinatesUtil.of((short) 7, (short) 17)));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordL, CoordinatesUtil.of((long) 20, (long) 10), CoordinatesUtil.of((long) 25, (long) 25)));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordI, INTCOORD01, INTCOORD02));
+        Assert.assertFalse(CoordinatesUtil.isValid(coordI, INTCOORD03, INTCOORD04));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordF, FLOATCOORD01, FLOATCOORD02));
+        Assert.assertFalse(CoordinatesUtil.isValid(coordF, FLOATCOORD03, FLOATCOORD04));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordD, DOUBLECOORD01, DOUBLECOORD02));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordS, SHORTCOORD01, SHORTCOORD02));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordL, LONGCOORD01, LONGCOORD02));
     }
 
     /**
@@ -48,12 +62,12 @@ class CoordinatesUtilTest {
         final var coordD = CoordinatesUtil.of(5.5, 10.5);
         final var coordS = CoordinatesUtil.of((short) 5, (short) 10);
         final var coordL = CoordinatesUtil.of((long) 20, (long) 10);
-        Assert.assertTrue(CoordinatesUtil.isValid(coordI, CoordinatesUtil.of(14, 29)));
-        Assert.assertFalse(CoordinatesUtil.isValid(coordI, CoordinatesUtil.of(4, 7)));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordF, CoordinatesUtil.of((float) 13.7, (float) 7.8)));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordD, CoordinatesUtil.of(6.9, 34.9)));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordS, CoordinatesUtil.of((short) 7, (short) 16)));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordL, CoordinatesUtil.of((long) 35, (long) 24)));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordI, INTCOORD02));
+        Assert.assertFalse(CoordinatesUtil.isValid(coordI, INTCOORD01));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordF, FLOATCOORD02));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordD, DOUBLECOORD02));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordS, SHORTCOORD02));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordL, LONGCOORD02));
     }
 
     /**
@@ -66,11 +80,11 @@ class CoordinatesUtilTest {
         final var coordD = CoordinatesUtil.of(5.5, 10.5);
         final var coordS = CoordinatesUtil.of((short) 5, (short) 10);
         final var coordL = CoordinatesUtil.of((long) 20, (long) 10);
-        Assert.assertTrue(CoordinatesUtil.isValid(coordI, 14, 8));
-        Assert.assertFalse(CoordinatesUtil.isValid(coordI, 5, 18));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordF, (float) 13.2, (float) 7.6));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordD, 7.3, 19.0));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordS, (short) 8, (short) 20));
-        Assert.assertTrue(CoordinatesUtil.isValid(coordL, (long) 34, (long) 18));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordI, INTCOORD02.getX(), INTCOORD02.getY()));
+        Assert.assertFalse(CoordinatesUtil.isValid(coordI, INTCOORD01.getX(), INTCOORD01.getY()));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordF, FLOATCOORD02.getX(), FLOATCOORD02.getY()));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordD, DOUBLECOORD02.getX(), DOUBLECOORD02.getY()));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordS, SHORTCOORD02.getX(), SHORTCOORD02.getY()));
+        Assert.assertTrue(CoordinatesUtil.isValid(coordL, LONGCOORD02.getX(), LONGCOORD02.getY()));
     }
 }
