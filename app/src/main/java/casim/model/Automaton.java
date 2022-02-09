2a7d9766468d4aa7e6451c321a7e5a21ff2b9390
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-02-09 10:50:02 +0100

    Add Automaton interface.

---
+++ app/src/main/java/casim/model/Automaton.java
@@ -0,0 +1,23 @@
+package casim.model;
+
+import casim.utils.grid.Grid;
+
+/**
+ *  An interface which describes an {@link Automaton}.
+ */
+public interface Automaton {
+
+    /**
+     * Method called to let the {@link Automaton} to do the next step.
+     */
+    void step();
+
+    /**
+     * Get the {@link Grid} represents the current {@link Automaton}'s state.
+     * 
+     * @param <T> the type of each cell of the {@link Grid} returned.
+     * @return {@link Grid} of type T, {@link Grid} describing the current {@link Automaton}'s state.
+     */
+    <T> Grid<T> getGrid();
+
+}
