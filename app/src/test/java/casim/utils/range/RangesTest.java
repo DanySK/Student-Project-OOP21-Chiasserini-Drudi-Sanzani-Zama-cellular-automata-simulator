b539baf980498bc12693b4f193ffe4d80f97729f
Filippo Sanzani <filippo.sanzani@studio.unibo.it>
2022-02-10 12:48:07 +0100

    Add Range utilities.

---
+++ app/src/test/java/casim/utils/range/RangesTest.java
@@ -0,0 +1,43 @@
+package casim.utils.range;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for {@link Ranges}.
+ */
+public class RangesTest {
+
+    /**
+     * Test for {@link Ranges#of(Comparable, Comparable, java.util.function.Function)} method. 
+     */
+    @Test
+    void testOfGeneric() {
+        fail();
+    }
+
+    /**
+     * Test for {@link Ranges#of(int, int, int))} method. 
+     */
+    @Test
+    void testOfIntegerWithStep() {
+        fail();
+    }
+
+    /**
+     * Test for {@link Ranges#of(int, int))} method. 
+     */
+    @Test
+    void testOfInteger() {
+        fail();
+    }
+
+    /**
+     * Test for {@link Ranges#of(double, double, double))} method. 
+     */
+    @Test
+    void testOfDouble() {
+        fail();
+    }
+}
