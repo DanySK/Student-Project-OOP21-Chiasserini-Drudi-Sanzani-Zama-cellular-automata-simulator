0f90a6dd2ae89da8ff9218e9052d4acffbd73dba
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-02-10 14:58:39 +0100

    Add getState method to CellAttributes.

--- app/src/main/java/casim/model/CellAttributes.java
+++ app/src/main/java/casim/model/CellAttributes.java
@@ -4,9 +4,18 @@ package casim.model;
  * An interface which describes the {@link Cell}'s attributes.
  * 
  * Each cell's attributes consist of:
- * - an enumeration represents the finite states a {@link Cell} can assume;
+ * - One of the finite states a {@link Cell} can assume;
  * - eventually other attributes as the life-time.
+ * 
+ * @param <T> the type of the finite states a cell can assume (usually they're represented by an enumeration).
  */
-public interface CellAttributes {
+public interface CellAttributes<T> {
+
+    /**
+     * Return the current state of the {@link Cell}. 
+     *
+     * @return the current state of the {@link Cell}.
+     */
+    T getState();
 
 }
