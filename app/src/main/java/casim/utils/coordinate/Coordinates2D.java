a0b0139483e4e8a5b519b3b41d60ea91e97f9ce9
Simone Zama <simone.zama@studio.unibo.it>
2022-02-14 15:17:37 +0100

    Change Coordinates interface and create Coordinates2D interface

---
+++ app/src/main/java/casim/utils/coordinate/Coordinates2D.java
@@ -0,0 +1,38 @@
+package casim.utils.coordinate;
+
+/**
+ * Represents a 2D Coordinate.
+ *
+ * @param <T> the type of the coordinate system.
+ */
+public interface Coordinates2D<T extends Number> extends Coordinates<T> {
+
+    /**
+     * Set the X coordinate value.
+     * 
+     * @param value the value to be assigned to the X.
+     */
+    void setX(T value);
+
+    /**
+     * Set the Y coordinate value.
+     * 
+     * @param value the value to be assigned to the Y.
+    */
+    void setY(T value);
+
+    /**
+     * Get the X coordinate value.
+     * 
+     * @return the X coordinate value.
+     */
+    T getX();
+
+    /**
+     * Get the Y coordinate value.
+     * 
+     * @return the Y coordinate value.
+     */
+    T getY();
+    
+}
