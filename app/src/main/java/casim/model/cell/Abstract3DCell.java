33c3e790060f28288e774fc68338836cce71b8e6
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-02-14 23:14:41 +0100

    Add getCoordinates to Abstract3DCell.

--- app/src/main/java/casim/model/cell/Abstract3DCell.java
+++ app/src/main/java/casim/model/cell/Abstract3DCell.java
@@ -1,11 +1,32 @@
 package casim.model.cell;
 
+import casim.utils.coordinate.Coordinates;
+import casim.utils.coordinate.Coordinates3D;
+
 /**
  * An abstract which describes a generic 3D {@link Cell}, it doesn't specify the finite states type.
  * 
  * @param <T> the type of the finite states of the {@link Cell}.
  */
 public abstract class Abstract3DCell<T> implements Cell<T> {
+
+    private final Coordinates<Integer> coordinates;
+
+    /**
+     * Construct a new {@link Coordinates3D}.
+     * 
+     * @param coordinates the coordinates of the cell.
+     */
+    public Abstract3DCell(final Coordinates3D<Integer> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public Coordinates<Integer> getCoordinates() {
+        return this.coordinates;
+    }
 
     /**
      * {@inheritDoc}
