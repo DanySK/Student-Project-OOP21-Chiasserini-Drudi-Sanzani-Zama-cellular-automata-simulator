34d83973e30ea7187fe90ee3feacd6c6ad6104a9
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-02-16 14:35:23 +0100

    Neighbors2DFunction partial implementation.

--- app/src/main/java/casim/model/rule/function/Neighbors2DFunction.java
+++ app/src/main/java/casim/model/rule/function/Neighbors2DFunction.java
@@ -1,19 +1,30 @@
 package casim.model.rule.function;
 
-import java.util.function.Function;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import casim.model.cell.implementations.Cell2D;
 import casim.model.cell.interfaces.Cell;
+import casim.utils.coordinate.CoordinatesUtil;
+import casim.utils.grid.Grid;
 
 /**
  * General function used to calculate neighbors of a {@link Cell} in a 2D grid.
  * 
  * @param <T> the type of the finite states a {@link Cell} can assume.
  */
-public class Neighbors2DFunction<T> implements Function<Cell<T>, Iterable<Cell<T>>> {
-
+public class Neighbors2DFunction<T> implements BiFunction<Cell2D<T>, Grid<Cell<T>>, Iterable<Cell<T>>> {
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public Iterable<Cell<T>> apply(final Cell<T> t) {
-        // TODO Auto-generated method stub
-        return null;
+    public Iterable<Cell<T>> apply(final Cell2D<T> cell, final Grid<Cell<T>> grid) {
+        return Stream.of(CoordinatesUtil.of(1, 0), CoordinatesUtil.of(0, 1), 
+                          CoordinatesUtil.of(0, -1), CoordinatesUtil.of(-1, 0))
+                     .map(coord -> CoordinatesUtil.sumInt(coord, cell.getCoordinates()))
+                     .map(coord -> new Cell2D<>(coord, cell.getAttributes()))
+                     .collect(Collectors.toList());
     }
 
 }
