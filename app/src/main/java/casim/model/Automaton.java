13d853f1cd29ab267122a4ecced9ebb8b2a63735
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-02-23 16:53:01 +0100

    Automaton refactor.

--- app/src/main/java/casim/model/Automaton.java
+++ app/src/main/java/casim/model/Automaton.java
@@ -3,21 +3,20 @@ package casim.model;
 import java.util.Iterator;
 
 import casim.model.cell.interfaces.Cell;
-import casim.utils.coordinate.Coordinates;
-import casim.utils.grid.Grid;
+import casim.utils.grid.Grid2D;
 
 /**
  *  An interface which describes an {@link Automaton}.
  *
  *  @param <T> the type of the finite states of the {@link Automaton}'s {@link Cell}.
  */
-public interface Automaton<T> extends Iterator<Grid<Coordinates<Integer>, Cell<T>>> {
+public interface Automaton<T> extends Iterator<Grid2D<Cell<T>>> {
 
     /**
      * Get the {@link Grid} describing the current {@link Automaton}'s state.
      * 
      * @return {@link Grid} A {@link Grid} of {@link Cell}.
      */
-    Grid<Coordinates<Integer>, Cell<T>> getGrid();
+    Grid2D<Cell<T>> getGrid();
 
 }
