2be6cdc5db1ccef9af4e8f7bedc50283b8559dcb
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-02-24 17:26:56 +0100

    Implementation of AbstractCell.

---
+++ app/src/main/java/casim/model/cell/AbstractCell.java
@@ -0,0 +1,29 @@
+package casim.model.cell;
+
+/**
+ * An abstract implementation of a {@Cell}.
+ * 
+ * @param <T> the type of the finite states a {@link Cell} can assume.
+ */
+public abstract class AbstractCell<T> implements Cell<T> {
+
+    private final T state;
+
+    /**
+     * Construct a new {@link AbstractCell}.
+     * 
+     * @param state the current state of the {@link Cell}.
+     */
+    public AbstractCell(final T state) {
+        this.state = state;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public T getState() {
+        return this.state;
+    }
+
+}
