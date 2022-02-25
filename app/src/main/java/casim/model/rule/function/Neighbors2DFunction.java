1c5919e1f28c9efe63d2a7faf6224a5ec65b2183
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-02-25 13:36:35 +0100

    Fix Neighbors2DFunction.

--- app/src/main/java/casim/model/rule/function/Neighbors2DFunction.java
+++ app/src/main/java/casim/model/rule/function/Neighbors2DFunction.java
@@ -4,28 +4,29 @@ import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import casim.model.cell.implementations.Cell2D;
-import casim.model.cell.interfaces.Cell;
-import casim.utils.coordinate.Coordinates;
+import org.apache.commons.lang3.tuple.Pair;
+
+import casim.model.cell.Cell;
+import casim.utils.coordinate.Coordinates2D;
 import casim.utils.coordinate.CoordinatesUtil;
-import casim.utils.grid.Grid;
+import casim.utils.grid.Grid2D;
 
 /**
  * General function used to calculate neighbors of a {@link Cell} in a 2D grid.
  * 
  * @param <T> the type of the finite states a {@link Cell} can assume.
  */
-public class Neighbors2DFunction<T> implements BiFunction<Cell2D<T>, Grid<Coordinates<Integer>, Cell<T>>, Iterable<Cell<T>>> {
+public class Neighbors2DFunction<T> implements BiFunction<Pair<Coordinates2D<Integer>, Cell<T>>, Grid2D<Cell<T>>, Iterable<Pair<Coordinates2D<Integer>, Cell<T>>>> {
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Iterable<Cell<T>> apply(final Cell2D<T> cell, final Grid<Coordinates<Integer>, Cell<T>> grid) {
+    public Iterable<Pair<Coordinates2D<Integer>, Cell<T>>> apply(final Pair<Coordinates2D<Integer>, Cell<T>> cellPair, final Grid2D<Cell<T>> grid) {
         return Stream.of(CoordinatesUtil.of(1, 0), CoordinatesUtil.of(0, 1), CoordinatesUtil.of(0, -1), CoordinatesUtil.of(-1, 0))
-            .map(coord -> CoordinatesUtil.sumInt(coord, cell.getCoordinates()))
+            .map(coord -> CoordinatesUtil.sumInt(coord, cellPair.getLeft()))
             .filter(grid::isCoordValid)
-            .map(coord -> new Cell2D<>(coord, grid.get(coord).getAttributes())) 
+            .map(coord -> Pair.of(coord, grid.get(coord))) 
             .collect(Collectors.toList());
     }
 
