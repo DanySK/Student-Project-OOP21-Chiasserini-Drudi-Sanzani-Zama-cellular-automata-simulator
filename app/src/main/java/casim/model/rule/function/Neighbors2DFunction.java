e7ec05bb2b7f1df44eacc49de43f589f5dcdcebf
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-02-25 15:25:56 +0100

    Fix Neighbors2DFunction.

--- app/src/main/java/casim/model/rule/function/Neighbors2DFunction.java
+++ app/src/main/java/casim/model/rule/function/Neighbors2DFunction.java
@@ -7,26 +7,27 @@ import java.util.stream.Stream;
 import org.apache.commons.lang3.tuple.Pair;
 
 import casim.model.cell.Cell;
+import casim.utils.coordinate.Coordinates;
 import casim.utils.coordinate.Coordinates2D;
 import casim.utils.coordinate.CoordinatesUtil;
-import casim.utils.grid.Grid2D;
+import casim.utils.grid.Grid;
 
 /**
  * General function used to get the neighbors of a {@link Cell} in a {@link Grid2D}.
  * 
  *  @param <T> the type of the finite states of the {@link Automaton}'s {@link Cell}.
  */
-public class Neighbors2DFunction<T> implements BiFunction<Pair<Coordinates2D<Integer>, Cell<T>>, Grid2D<Cell<T>>, Iterable<Pair<Coordinates2D<Integer>, Cell<T>>>> {
+public class Neighbors2DFunction<T> implements BiFunction<Pair<Coordinates<Integer>, Cell<T>>, Grid<Coordinates<Integer>, Cell<T>>, Iterable<Pair<Coordinates<Integer>, Cell<T>>>> {
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Iterable<Pair<Coordinates2D<Integer>, Cell<T>>> apply(final Pair<Coordinates2D<Integer>, Cell<T>> cellPair, final Grid2D<Cell<T>> grid) {
+    public Iterable<Pair<Coordinates<Integer>, Cell<T>>> apply(final Pair<Coordinates<Integer>, Cell<T>> cellPair, final Grid<Coordinates<Integer>, Cell<T>> grid) {
         return Stream.of(CoordinatesUtil.of(1, 0), CoordinatesUtil.of(0, 1), CoordinatesUtil.of(0, -1), CoordinatesUtil.of(-1, 0))
-            .map(coord -> CoordinatesUtil.sumInt(coord, cellPair.getLeft()))
+            .map(coord -> CoordinatesUtil.sumInt(coord, (Coordinates2D<Integer>) cellPair.getLeft()))
             .filter(grid::isCoordValid)
-            .map(coord -> Pair.of(coord, grid.get(coord))) 
+            .map(coord -> Pair.of((Coordinates<Integer>) coord, grid.get(coord))) 
             .collect(Collectors.toList());
     }
 
