5b77469a18f8eca4c5c904088e39ad167b0ff17e
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-02-25 12:45:12 +0100

    Change implementation of AutomatonGridFactory.

--- app/src/main/java/casim/model/gridfactory/AutomatonGridFactory.java
+++ app/src/main/java/casim/model/gridfactory/AutomatonGridFactory.java
@@ -1,33 +1,37 @@
 package casim.model.gridfactory;
 
-import casim.model.cell.interfaces.Cell;
+import java.util.function.Supplier;
+
+import casim.model.cell.Cell;
 import casim.utils.grid.Grid2D;
 import casim.utils.grid.Grid3D;
 
 /**
  * A Builder for a {@link Grid} of elements of type T.
  * 
- * @param <T> type of the elements contained in the {@link Grid}.
+ *  @param <T> the type of the finite states of the {@link Automaton}'s {@link Cell}.
  */
 public interface AutomatonGridFactory<T> {
 
     /**
      * Return a new 2D {@link Grid} of elements of type T.
      * 
-     * @param width the width of the grid
-     * @param height the height of the grid
+     * @param width the width of the grid;
+     * @param height the height of the grid;
+     * @param cellSupplier a supplier of {@link Cell} to generate the grid.
      * @return a new {@link Grid} of type T.
      */
-    Grid2D<Cell<T>> create2DGrid(int width, int height); 
+    Grid2D<Cell<T>> create2DGrid(int width, int height, Supplier<Cell<T>> cellSupplier); 
 
     /**
      * Return a new 3D {@link Grid} of elements of type T.
      * 
-     * @param width the width of the grid
-     * @param height the height of the grid
-     * @param depth the depth of the grid
+     * @param width the width of the grid;
+     * @param height the height of the grid;
+     * @param depth the depth of the grid;
+     * @param cellSupplier a supplier of {@link Cell} to generate the grid.
      * @return a new {@link Grid} of type T.
      */
-    Grid3D<Cell<T>> create3DGrid(int width, int height, int depth);
+    Grid3D<Cell<T>> create3DGrid(int width, int height, int depth, Supplier<Cell<T>> cellSupplier);
 
 }
