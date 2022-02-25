7948c18cdc5f30ea6fe8eafd6d3fd73e8be671b1
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-02-25 13:41:38 +0100

    Fix Neighbors3DFunction.

--- app/src/main/java/casim/model/rule/function/Neighbors3DFunction.java
+++ app/src/main/java/casim/model/rule/function/Neighbors3DFunction.java
@@ -4,29 +4,30 @@ import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import casim.model.cell.implementations.Cell3D;
-import casim.model.cell.interfaces.Cell;
-import casim.utils.coordinate.Coordinates;
+import org.apache.commons.lang3.tuple.Pair;
+
+import casim.model.cell.Cell;
+import casim.utils.coordinate.Coordinates3D;
 import casim.utils.coordinate.CoordinatesUtil;
-import casim.utils.grid.Grid;
+import casim.utils.grid.Grid3D;
 
 /**
  * General function used to calculate neighbors of a {@link Cell} in a 3D grid.
  * 
  * @param <T> the type of the finite states a {@link Cell} can assume.
  */
-public class Neighbors3DFunction<T> implements BiFunction<Cell3D<T>, Grid<Coordinates<Integer>, Cell<T>>, Iterable<Cell<T>>> {
+public class Neighbors3DFunction<T> implements BiFunction<Pair<Coordinates3D<Integer>, Cell<T>>, Grid3D<Cell<T>>, Iterable<Pair<Coordinates3D<Integer>, Cell<T>>>> {
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Iterable<Cell<T>> apply(final Cell3D<T> cell, final Grid<Coordinates<Integer>, Cell<T>> grid) {
+    public Iterable<Pair<Coordinates3D<Integer>, Cell<T>>> apply(final Pair<Coordinates3D<Integer>, Cell<T>> cellPair, final Grid3D<Cell<T>> grid) {
         return Stream.of(CoordinatesUtil.of(1, 0, 0), CoordinatesUtil.of(-1, 0, 0), CoordinatesUtil.of(0, 1, 0),
                 CoordinatesUtil.of(0, -1, 0), CoordinatesUtil.of(0, 0, 1), CoordinatesUtil.of(0, 0, -1))
-            .map(coord -> CoordinatesUtil.sumInt(coord, cell.getCoordinates()))
+            .map(coord -> CoordinatesUtil.sumInt(coord, cellPair.getLeft()))
             .filter(grid::isCoordValid)
-            .map(coord -> new Cell3D<>(coord, grid.get(coord).getAttributes()))
+            .map(coord -> Pair.of(coord, grid.get(coord)))
             .collect(Collectors.toList());
     }
 
