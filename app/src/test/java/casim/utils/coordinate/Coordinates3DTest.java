672efa302d374bd5edccb198265f86e0c3b0bd64
Simone Zama <simone.zama@studio.unibo.it>
2022-03-03 09:51:05 +0100

    Create Coordinates3DTest

---
+++ app/src/test/java/casim/utils/coordinate/Coordinates3DTest.java
@@ -0,0 +1,39 @@
+package casim.utils.coordinate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test for {@link Coordinates3D}.
+ */
+public class Coordinates3DTest {
+
+    static final int XVALUE = 5;
+    static final int YVALUE = 10;
+    static final int ZVALUE = 15;
+    static final Coordinates3D<Integer> coord = CoordinatesUtil.of(XVALUE, YVALUE, ZVALUE);
+    static final Coordinates3D<Integer> coord01 = CoordinatesUtil.of(XVALUE, YVALUE, ZVALUE);
+
+    /**
+     * Test for {@link Coordinates3D#getX()} method.
+     */
+    @Test
+    void testGetX() {
+        Assert.assertEquals(XVALUE, (int)coord.getX());
+    }
+
+    /**
+     * Test for {@link Coordinates3D#getY()} method.
+     */
+    @Test
+    void testGetY() {
+        Assert.assertEquals(YVALUE, (int)coord.getY());
+    }
+
+    /**
+     * Test for {@link Coordinates3D#getZ()} method.
+     */
+    void testGetZ() {
+        Assert.assertEquals(ZVALUE, (int)coord.getZ());
+    }
+}
