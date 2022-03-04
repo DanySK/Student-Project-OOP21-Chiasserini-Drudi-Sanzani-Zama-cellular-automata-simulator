f1e91cb6e4d596872984f498a5bcef86cf74a206
Simone Zama <simone.zama@studio.unibo.it>
2022-03-04 14:53:44 +0100

    Fix PMD/Checkstyle errors

--- app/src/test/java/casim/utils/coordinate/Coordinates2DTest.java
+++ app/src/test/java/casim/utils/coordinate/Coordinates2DTest.java
@@ -10,14 +10,14 @@ class Coordinates2DTest {
 
     static final int XVALUE = 5;
     static final int YVALUE = 10;
-    static final Coordinates2D<Integer> coord = CoordinatesUtil.of(5,  10);
+    static final Coordinates2D<Integer> COORD = CoordinatesUtil.of(5,  10);
 
     /**
      * Test for {@link Coordinates2D#getX()} method. 
      */
     @Test
     void testGetX() {
-        Assert.assertEquals((int) coord.getX(), XVALUE);
+        Assert.assertEquals((int) COORD.getX(), XVALUE);
     }
 
     /**
@@ -25,7 +25,7 @@ class Coordinates2DTest {
      */
     @Test
     void testGetY() {
-        Assert.assertEquals((int) coord.getY(), YVALUE);
+        Assert.assertEquals((int) COORD.getY(), YVALUE);
     }
 
     /**
@@ -33,10 +33,10 @@ class Coordinates2DTest {
      */
     @Test
     void testEquals() {
-        Assert.assertTrue(coord.equals(coord));
+        Assert.assertTrue(COORD.equals(COORD));
         var coord01 = CoordinatesUtil.of(XVALUE, YVALUE);
-        Assert.assertTrue(coord.equals(coord01));
+        Assert.assertTrue(COORD.equals(coord01));
         coord01 = CoordinatesUtil.of(YVALUE, XVALUE);
-        Assert.assertFalse(coord.equals(coord01));
+        Assert.assertFalse(COORD.equals(coord01));
     }
 }
