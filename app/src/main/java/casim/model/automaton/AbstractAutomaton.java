86abee73311338b85c8656b629fa6f9e875303c8
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-03-07 17:29:40 +0100

    Implementation of AbstractAutomaton.

--- app/src/main/java/casim/model/automaton/AbstractAutomaton.java
+++ app/src/main/java/casim/model/automaton/AbstractAutomaton.java
@@ -1,7 +1,10 @@
 package casim.model.automaton;
 
+import java.util.EnumMap;
+
 import casim.model.cell.AbstractCell;
 import casim.model.utils.Stats;
+import casim.model.utils.StatsImpl;
 import casim.utils.grid.Grid2D;
 
 /**
@@ -9,30 +12,29 @@ import casim.utils.grid.Grid2D;
  *
  * @param <T> the enumeration which contains the finite states of the {@link Automaton}'s {@link Cell}.
  */
-public class AbstractAutomaton<T extends Enum<T>> implements Automaton<T> {
+public abstract class AbstractAutomaton<T extends Enum<T>> implements Automaton<T> {
+
+    private int iterationCounter;
 
     @Override
-    public boolean hasNext() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+    public abstract boolean hasNext();
 
     @Override
     public Grid2D<AbstractCell<T>> next() {
-        // TODO Auto-generated method stub
-        return null;
+        this.iterationCounter++;
+        return this.doStep();
     }
 
+    abstract Grid2D<AbstractCell<T>> doStep();
+
     @Override
-    public Grid2D<AbstractCell<T>> getGrid() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    public abstract Grid2D<AbstractCell<T>> getGrid();
 
     @Override
     public Stats<T> getStats() {
-        // TODO Auto-generated method stub
-        return null;
+        return new StatsImpl<T>(iterationCounter, this.createEnumMap());
     }
+
+    abstract EnumMap<T, Integer> createEnumMap();
 
 }
