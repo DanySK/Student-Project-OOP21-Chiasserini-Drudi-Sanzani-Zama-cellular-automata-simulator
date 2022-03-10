4ff79dddffd84d0460981ab17e644836931a45f2
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-03-10 10:15:56 +0100

    UpdateRule javadoc fix.

--- app/src/main/java/casim/model/abstraction/rule/UpdateRule.java
+++ app/src/main/java/casim/model/abstraction/rule/UpdateRule.java
@@ -3,19 +3,19 @@ package casim.model.abstraction.rule;
 import org.apache.commons.lang3.tuple.Pair;
 
 import casim.model.abstraction.cell.AbstractCell;
-import casim.model.abstraction.cell.Cell;
 import casim.utils.coordinate.Coordinates;
 import casim.utils.grid.Grid;
 
 /**
- * The {@link casim.model.Automaton}'s rule used to update the {@link Cell}'s state.
+ * The {@link casim.model.abstraction.Automaton}'s rule used to update the {@link casim.model.abstraction.cell.Cell}'s state.
  * 
- *  @param <T> the enumeration which contains the finite states of the {@link casim.model.Automaton}'s {@link Cell}.
+ *  @param <T> the {@link AbstractCell} implementation to update.
+ *  @param <C> the {@link Coordinates} implementation used by the cell (it can be 2D or 3D).
  */
 public interface UpdateRule<C extends Coordinates<? extends Number>, T extends AbstractCell<?>> {
 
      /**
-     * Return the updated {@link Cell}.
+     * Return the updated {@link casim.model.abstraction.cell.Cell}.
      * 
      * @param cellPair pair of {@link Coordinates} and {@link Cell} to update;
      * @param grid the {@link Grid} representing the {@link Automaton}.
