dd535c68925af430474447186825fe3d85e91941
Simone Zama <simone.zama@studio.unibo.it>
2022-03-14 20:46:27 +0100

    Remove ant field

--- app/src/main/java/casim/model/langtonsant/LangtonsAntCell.java
+++ app/src/main/java/casim/model/langtonsant/LangtonsAntCell.java
@@ -1,21 +1,10 @@
 package casim.model.langtonsant;
 
-import java.util.Optional;
 import casim.model.abstraction.cell.AbstractCell;
 
 public class LangtonsAntCell extends AbstractCell<CellState>{
 
-    private Optional<Ant> ant = Optional.empty();
-
     public LangtonsAntCell(CellState state) {
         super(state);
-    }
-    
-    public boolean hasAnt() {
-        return this.ant.isPresent();
-    }
-
-    public Ant getAnt() {
-        return this.ant.get();
     }
 }
