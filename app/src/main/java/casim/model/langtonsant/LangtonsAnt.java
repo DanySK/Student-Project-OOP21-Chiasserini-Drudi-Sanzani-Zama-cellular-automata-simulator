e5397c1260a1c0ff8e38b073b8a0a525f4bdd902
Simone Zama <simone.zama@studio.unibo.it>
2022-03-14 20:47:38 +0100

    Add incomplete implementation

--- app/src/main/java/casim/model/langtonsant/LangtonsAnt.java
+++ app/src/main/java/casim/model/langtonsant/LangtonsAnt.java
@@ -3,7 +3,6 @@ package casim.model.langtonsant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
-
 import casim.model.abstraction.automaton.AbstractAutomaton;
 import casim.utils.grid.Grid2D;
 import casim.utils.grid.Grid2DImpl;
@@ -25,14 +24,27 @@ public class LangtonsAnt extends AbstractAutomaton<CellState, LangtonsAntCell>{
     @Override
     protected Grid2D<LangtonsAntCell> doStep() {
         final var newState = new Grid2DImpl<LangtonsAntCell>(this.state.getHeight(), this.state.getWidth());
-        // TODO complete
-        return null;
+
+        this.ants.stream()
+            .forEach(x -> this.antStep(x, newState));
+        return newState;
+    }
+
+    private void antStep(final Ant ant, final Grid2DImpl<LangtonsAntCell> state) {
+        if (state.get(ant.getPosition()).getState() == CellState.OFF) {
+            state.set(ant.getPosition(), new LangtonsAntCell(CellState.ON));
+            // TODO change ant direction
+        } else if (state.get(ant.getPosition()).getState() == CellState.ON) {
+            state.set(ant.getPosition(), new LangtonsAntCell(CellState.OFF));
+            // TODO change ant direction
+        }
+        // TODO move ant towards facing direction  
     }
 
     @Override
     public Grid2D<LangtonsAntCell> getGrid() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.state;
+        // TODO return unmodifiable copy
     }
     
 }
