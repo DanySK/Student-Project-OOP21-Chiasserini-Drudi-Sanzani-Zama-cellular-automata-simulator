de811e94d3faab9076a4a1f1c4592a376df3ce59
Simone Zama <simone.zama@studio.unibo.it>
2022-03-14 16:36:30 +0100

    Create `LangtonsAntCell`

---
+++ app/src/main/java/casim/model/langtonsant/LangtonsAntCell.java
@@ -0,0 +1,11 @@
+package casim.model.langtonsant;
+
+import casim.model.abstraction.cell.AbstractCell;
+
+public class LangtonsAntCell extends AbstractCell<CellState>{
+
+    public LangtonsAntCell(CellState state) {
+        super(state);
+    }
+    
+}
