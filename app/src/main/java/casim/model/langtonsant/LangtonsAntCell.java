c5b3ec4eba6f7b2a272b4180cd100c782a428117
Simone Zama <simone.zama@studio.unibo.it>
2022-03-14 17:03:14 +0100

    Add `ant` field and setter/getter

--- app/src/main/java/casim/model/langtonsant/LangtonsAntCell.java
+++ app/src/main/java/casim/model/langtonsant/LangtonsAntCell.java
@@ -1,11 +1,21 @@
 package casim.model.langtonsant;
 
+import java.util.Optional;
 import casim.model.abstraction.cell.AbstractCell;
 
 public class LangtonsAntCell extends AbstractCell<CellState>{
+
+    private Optional<Ant> ant = Optional.empty();
 
     public LangtonsAntCell(CellState state) {
         super(state);
     }
     
+    public boolean hasAnt() {
+        return this.ant.isPresent();
+    }
+
+    public Ant getAnt() {
+        return this.ant.get();
+    }
 }
