ab4ec1bf4aa9abd33697aa4c7fd064c7ce940a3d
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-03-14 23:19:06 +0100

    Implementation of `CoDiCell`.

---
+++ app/src/main/java/casim/model/codi/CoDiCell.java
@@ -0,0 +1,19 @@
+package casim.model.codi;
+
+import casim.model.abstraction.cell.AbstractCell;
+
+/**
+ * The CoDi cells.
+ */
+public class CoDiCell extends AbstractCell<CellState> {
+
+    /**
+     * Constructor of {@link CoDiCell}.
+     * 
+     * @param state the state of the cell.
+     */
+    public CoDiCell(final CellState state) {
+        super(state);
+    }
+
+}
