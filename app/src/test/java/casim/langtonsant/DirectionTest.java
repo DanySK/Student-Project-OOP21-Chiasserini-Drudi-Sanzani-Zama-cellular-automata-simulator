dbc62dab643d68f97c7834909d415c524e8b1f51
Simone Zama <simone.zama@studio.unibo.it>
2022-03-15 09:33:37 +0100

    Update `DirectionTest`

--- app/src/test/java/casim/langtonsant/DirectionTest.java
+++ app/src/test/java/casim/langtonsant/DirectionTest.java
@@ -7,9 +7,13 @@ import casim.model.langtonsant.Direction;
 
 public class DirectionTest {
 
+    /**
+     * Test for {@link Direction#turn(Direction, CellState)} method.
+     */
     @Test
     void testTurn() {
         Assert.assertEquals(Direction.EAST, Direction.turn(Direction.NORTH, CellState.OFF));
         Assert.assertEquals(Direction.WEST, Direction.turn(Direction.NORTH, CellState.ON));
+        Assert.assertEquals(Direction.NORTH, Direction.turn(Direction.WEST, CellState.OFF));
     }
 }
