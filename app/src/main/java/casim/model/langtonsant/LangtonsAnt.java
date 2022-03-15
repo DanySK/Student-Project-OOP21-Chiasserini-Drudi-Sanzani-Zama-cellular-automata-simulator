4cc92f19fc19d90830b738f159b384999a6a0a0e
Simone Zama <simone.zama@studio.unibo.it>
2022-03-15 10:43:23 +0100

    Improve `doStep`

--- app/src/main/java/casim/model/langtonsant/LangtonsAnt.java
+++ app/src/main/java/casim/model/langtonsant/LangtonsAnt.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import casim.model.abstraction.automaton.AbstractAutomaton;
 import casim.utils.grid.Grid2D;
-import casim.utils.grid.Grid2DImpl;
 
 public class LangtonsAnt extends AbstractAutomaton<CellState, LangtonsAntCell>{
 
     private final List<Ant> ants = new ArrayList<Ant>();
-    private Grid2D<LangtonsAntCell> state;
+    private final Grid2D<LangtonsAntCell> state;
 
     public LangtonsAnt(final Grid2D<CellState> state) {
         this.state = state.map(x -> new LangtonsAntCell(x));
@@ -22,20 +21,18 @@ public class LangtonsAnt extends AbstractAutomaton<CellState, LangtonsAntCell>{
 
     @Override
     protected Grid2D<LangtonsAntCell> doStep() {
-        final var newState = new Grid2DImpl<LangtonsAntCell>(this.state.getHeight(), this.state.getWidth());
-
         this.ants.stream()
-            .forEach(x -> this.antStep(x, newState));
+            .forEach(x -> this.antStep(x));
         this.removeAnts();
-        return newState;
+        return this.state;
     }
 
-    private void antStep(final Ant ant, final Grid2DImpl<LangtonsAntCell> state) {
+    private void antStep(final Ant ant) {
         // Change the direction of the ant based on the state of its cell
         ant.setDirection(Direction.turn(ant.getDirection(), state.get(ant.getPosition()).getState()));
         // Change state of the cell the ant is on
-        state.set(ant.getPosition(),
-                new LangtonsAntCell(state.get(ant.getPosition()).getState() == CellState.OFF ? CellState.ON : CellState.OFF));
+        this.state.set(ant.getPosition(),
+                new LangtonsAntCell(this.state.get(ant.getPosition()).getState() == CellState.OFF ? CellState.ON : CellState.OFF));
         // Move the ant in the direction it is facing
         ant.move();
     }
