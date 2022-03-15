5e41eb9ddb1f7158154faea9ffcb4c8884acfcb0
Simone Zama <simone.zama@studio.unibo.it>
2022-03-15 09:09:39 +0100

    Add `turn` method

--- app/src/main/java/casim/model/langtonsant/Direction.java
+++ app/src/main/java/casim/model/langtonsant/Direction.java
@@ -5,4 +5,15 @@ public enum Direction {
     EAST,
     SOUTH,
     WEST;
+
+    /**
+     * Given a {@link Direction} and a {@link CellState} returns the new altered {@link Direction}
+     * 
+     * @param direction current {@link Direction} to be altered
+     * @param state current {@link CellState} of the {@link LangtonsAntCell} the {@link Ant} is on.
+     * @return a new {@link Direction} altered based on the {@link CellState}
+     */
+    public static Direction turn(final Direction direction, final CellState state) {
+        return Direction.values()[direction.ordinal() + (state == CellState.ON ? -1 : +1) % Direction.values().length];
+    }
 }
