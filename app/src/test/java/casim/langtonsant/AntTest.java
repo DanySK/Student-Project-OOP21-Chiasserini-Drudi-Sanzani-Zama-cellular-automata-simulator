2fd93668a99ccd2c766021648467d7e72594fea3
Simone Zama <simone.zama@studio.unibo.it>
2022-03-15 18:42:55 +0100

    Change tests to move `turn` method into `Ant`

--- app/src/test/java/casim/langtonsant/AntTest.java
+++ app/src/test/java/casim/langtonsant/AntTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import casim.model.langtonsant.Ant;
+import casim.model.langtonsant.CellState;
 import casim.model.langtonsant.Direction;
 import casim.utils.coordinate.Coordinates2D;
 import casim.utils.coordinate.CoordinatesUtil;
@@ -13,24 +14,41 @@ import casim.utils.coordinate.CoordinatesUtil;
  */
 public class AntTest {
     
-    private static final Coordinates2D<Integer> startingPosition = CoordinatesUtil.of(0, 0);
-    private static final Coordinates2D<Integer> northResult = CoordinatesUtil.of(0, 1);
-    private static final Coordinates2D<Integer> eastResult = CoordinatesUtil.of(1, 0);
-
+    private static final Coordinates2D<Integer> STARTINGPOSITION = CoordinatesUtil.of(0, 0);
+    private static final Coordinates2D<Integer> NORTHRESULT = CoordinatesUtil.of(0, 1);
+    private static final Coordinates2D<Integer> EASTRESULT = CoordinatesUtil.of(1, 0);
+    private static final Ant ANT = new Ant(Direction.NORTH, STARTINGPOSITION);
+    
     /**
      * Test for {@link Ant#move()} method.
      */
     @Test
     void testMove() {
-        final var ant = new Ant(Direction.NORTH, startingPosition);
-        ant.move();
-        Assert.assertEquals(northResult, ant.getPosition());
-        ant.setDirection(Direction.SOUTH);
-        ant.move();
-        Assert.assertEquals(startingPosition, ant.getPosition());
-        ant.setDirection(Direction.EAST);
-        ant.move();
-        Assert.assertEquals(eastResult, ant.getPosition());
+        ANT.move();
+        Assert.assertEquals(NORTHRESULT, ANT.getPosition());
+        ANT.setDirection(Direction.SOUTH);
+        ANT.move();
+        Assert.assertEquals(STARTINGPOSITION, ANT.getPosition());
+        ANT.setDirection(Direction.EAST);
+        ANT.move();
+        Assert.assertEquals(EASTRESULT, ANT.getPosition());
+    }
+    
+    
+    /**
+     * Test for {@link Ant#turn(CellState)} method.
+     */
+    @Test
+    void testTurn() {
+        ANT.setDirection(Direction.NORTH);
+        ANT.turn(CellState.OFF);
+        Assert.assertEquals(Direction.EAST, ANT.getDirection());
+        ANT.setDirection(Direction.NORTH);
+        ANT.turn(CellState.ON);
+        Assert.assertEquals(Direction.WEST, ANT.getDirection());
+        ANT.setDirection(Direction.WEST);
+        ANT.turn(CellState.OFF);
+        Assert.assertEquals(Direction.NORTH, ANT.getDirection());
     }
 
 }
