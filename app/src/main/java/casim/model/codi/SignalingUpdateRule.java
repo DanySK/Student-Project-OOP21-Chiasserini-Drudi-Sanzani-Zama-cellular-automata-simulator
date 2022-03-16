bd268ed5c1eb5cbfc00971f49cad8540aeeedec1
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-03-16 19:43:23 +0100

    Implementation of `SignalingUpdateRule`.

--- app/src/main/java/casim/model/codi/SignalingUpdateRule.java
+++ app/src/main/java/casim/model/codi/SignalingUpdateRule.java
@@ -14,6 +14,8 @@ import casim.utils.grid.Grid;
  */
 public class SignalingUpdateRule extends AbstractUpdateRule<Coordinates3D<Integer>, CoDiCell> {
 
+    private static final int NEURON_ACTIVATION_VALUE = 31;
+
     /**
      * Constructor of {@link SignalingUpdateRule}.
      * 
@@ -29,8 +31,44 @@ public class SignalingUpdateRule extends AbstractUpdateRule<Coordinates3D<Intege
     @Override
     protected CoDiCell nextCell(final Pair<Coordinates3D<Integer>, CoDiCell> cellPair,
             final List<Pair<Coordinates3D<Integer>, CoDiCell>> neighborsPairs) {
-        // TODO Auto-generated method stub
-        return null;
+        int inputCounter;
+        final CoDiCell cell = cellPair.getRight();
+        switch (cell.getState()) {
+            case BLANK:
+                break;
+            case NEURON:
+                inputCounter = 1 + cell.getNeighborsPriorInput().values().stream().reduce((n1, n2) -> n1 + n2).get()
+                        - cell.getNeighborsPriorInput().get(cell.getOppositeToGate().get())
+                        - cell.getNeighborsPriorInput().get(cell.getGate().get());
+                cell.setActivationCounter(inputCounter);
+                this.setPriorInputToValue(cell, 0);
+                if (cell.getActivationCounter() > NEURON_ACTIVATION_VALUE) {
+                   cell.getNeighborsPriorInput().put(cell.getGate().get(), 1);
+                   cell.getNeighborsPriorInput().put(cell.getOppositeToGate().get(), 1);
+                   cell.setActivationCounter(0);
+                }
+                break;
+            case AXON:
+                this.setPriorInputToValue(cell, cell.getActivationCounter());
+                cell.setActivationCounter((cell.getNeighborsPriorInput().get(cell.getGate().get()) != 0) ? 1 : 0);
+                break;
+            case DENTRITE:
+                inputCounter = cell.getNeighborsPriorInput().values().stream().reduce((n1, n2) -> n1 + n2).get();
+                inputCounter = inputCounter > 2 ? 2 : inputCounter;
+                this.setPriorInputToValue(cell, 0);
+                cell.getNeighborsPriorInput().put(cell.getGate().get(), inputCounter);
+                cell.setActivationCounter((inputCounter != 0) ? 1 : 0);
+                break;
+            default:
+                break;
+            }
+        return null; //TODO don't change the cell value but return the new cell
+    }
+
+    private void setPriorInputToValue(final CoDiCell cell, final int value) {
+        for (final var d: Direction.values()) {
+            cell.getNeighborsPriorInput().put(d, value);
+        }
     }
 
 }
