7b6d979d5bd4c034a4fdba00f6957e5b2f0904a5
Simone Zama <simone.zama@studio.unibo.it>
2022-03-18 19:07:25 +0100

    Improve `UpdateRule`

--- app/src/main/java/casim/model/wator/UpdateRule.java
+++ app/src/main/java/casim/model/wator/UpdateRule.java
@@ -15,7 +15,6 @@ import casim.utils.grid.Grid;
 public class UpdateRule extends AbstractUpdateRule<Coordinates2D<Integer>, WatorCell>{
 
     private static final String UNKNOW_STATE = "Cell has unknown state.";
-    private static final int NEWBORN_HEALTH = 1;
     private static final int DEAD_HEALTH = 0;
 
     public UpdateRule(
@@ -27,20 +26,15 @@ public class UpdateRule extends AbstractUpdateRule<Coordinates2D<Integer>, Wator
     protected WatorCell nextCell(Pair<Coordinates2D<Integer>, WatorCell> cellPair,
             List<Pair<Coordinates2D<Integer>, WatorCell>> neighborsPairs) {
         final var cell = cellPair.getRight();
+        final var newCell = cell.reproduce();
         switch (cellPair.getRight().getState()) {
             case PREY:
                 if (this.countState(neighborsPairs, CellState.DEAD) > 0) {
                     //move and reproduce if can
                     final var deadNeighbors = this.getFilteredList(neighborsPairs, CellState.DEAD);
-                    // create newborn if at max health
-                    WatorCell newCell = new WatorCell(CellState.DEAD, DEAD_HEALTH, cell.getMaxHealth());
-                    if (cell.getHealth() == cell.getMaxHealth()) {
-                        newCell = new WatorCell(CellState.PREY, NEWBORN_HEALTH, cell.getMaxHealth());
-                        cell.setHealth(NEWBORN_HEALTH);
-                    }
                     // move current prey to new position and heal by 1
                     var toChange = this.getCellToChange(deadNeighbors);
-                    this.changeCell(toChange, CellState.PREY, cell.getHealth(), cell.getMaxHealth(), WatorCell::heal);
+                    this.move(toChange, CellState.PREY, cell.getHealth(), cell.getMaxHealth(), WatorCell::heal);
                     return newCell;
                 } else {
                     // don't move
@@ -48,23 +42,24 @@ public class UpdateRule extends AbstractUpdateRule<Coordinates2D<Integer>, Wator
                     return cell;
                 }
             case PREDATOR:
-                if (cell.isDead()) {
-                    return new WatorCell(CellState.DEAD, DEAD_HEALTH, cell.getMaxHealth());
-                } else if (this.countState(neighborsPairs, CellState.PREY) > 0) {
+                if (this.countState(neighborsPairs, CellState.PREY) > 0) {
                     // move and eat
                     final var preyNeighbors = this.getFilteredList(neighborsPairs, CellState.PREY);
                     var toChange = this.getCellToChange(preyNeighbors);
-                    this.changeCell(toChange, CellState.PREDATOR, cell.getHealth(), cell.getMaxHealth(), WatorCell::heal);
-                    return new WatorCell(CellState.DEAD, DEAD_HEALTH, cell.getMaxHealth());
+                    this.move(toChange, CellState.PREDATOR, cell.getHealth(), cell.getMaxHealth(), WatorCell::heal);
+                    return newCell;
                 } else if (this.countState(neighborsPairs, CellState.DEAD) > 0) {
                     // move and lose health
                     final var deadNeighbors = this.getFilteredList(neighborsPairs, CellState.DEAD);
                     var toChange = this.getCellToChange(deadNeighbors);
-                    this.changeCell(toChange, CellState.PREDATOR, cell.getHealth(), cell.getMaxHealth(), WatorCell::starve);
-                    return new WatorCell(CellState.DEAD, DEAD_HEALTH, cell.getMaxHealth());
+                    this.move(toChange, CellState.PREDATOR, cell.getHealth(), cell.getMaxHealth(), WatorCell::starve);
+                    return newCell;
                 } else {
                     // don't move and lose health
                     cell.starve();
+                    if (cell.isDead()) {
+                        return new WatorCell(CellState.DEAD, DEAD_HEALTH, cell.getMaxHealth());
+                    }
                     return cell;
                 }
             case DEAD:
@@ -75,10 +70,14 @@ public class UpdateRule extends AbstractUpdateRule<Coordinates2D<Integer>, Wator
         }
     }
     
-    private void changeCell(WatorCell toChange, final CellState state, final int health, final int maxHealth,
+    private void move(WatorCell toChange, final CellState state, final int health, final int maxHealth,
             Consumer<WatorCell> consumer) {
-        toChange = new WatorCell(state, health, maxHealth);
+        var newCell = new WatorCell(state, health, maxHealth);
         consumer.accept(toChange);
+        if (newCell.isDead()) {
+            newCell = new WatorCell(CellState.DEAD, DEAD_HEALTH, maxHealth);
+        }
+        toChange = newCell;
     }
 
     private WatorCell getCellToChange(final List<WatorCell> list) {
