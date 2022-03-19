58cdc69cac0ca9100334d8bd61bebb8b34db432a
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-03-19 17:49:24 +0100

    Fix `GrowthUpdateRule`.

--- app/src/main/java/casim/model/codi/rule/GrowthUpdateRule.java
+++ app/src/main/java/casim/model/codi/rule/GrowthUpdateRule.java
@@ -1,11 +1,18 @@
-package casim.model.codi;
+package casim.model.codi.rule;
 
+import java.util.EnumMap;
 import java.util.List;
 import java.util.function.BiFunction;
 
 import org.apache.commons.lang3.tuple.Pair;
 
 import casim.model.abstraction.rule.AbstractUpdateRule;
+import casim.model.codi.cell.CoDiCell;
+import casim.model.codi.cell.attributes.CellState;
+import casim.model.codi.cell.attributes.Direction;
+import casim.model.codi.cell.attributes.Signal;
+import casim.model.codi.cell.builder.CoDiCellBuilder;
+import casim.model.codi.cell.builder.CoDiCellBuilderImpl;
 import casim.utils.coordinate.Coordinates3D;
 import casim.utils.grid.Grid;
 
@@ -29,8 +36,91 @@ public class GrowthUpdateRule extends AbstractUpdateRule<Coordinates3D<Integer>,
     @Override
     protected CoDiCell nextCell(final Pair<Coordinates3D<Integer>, CoDiCell> cellPair,
             final List<Pair<Coordinates3D<Integer>, CoDiCell>> neighborsPairs) {
-        // TODO Auto-generated method stub
-        return null;
+        final CoDiCell cell = cellPair.getRight();
+        final CoDiCellBuilder builder = new CoDiCellBuilderImpl();
+        EnumMap<Direction, Integer> neighborsPreviousInput = cell.getNeighborsPreviousInput();
+        switch (cell.getState()) {
+            case BLANK:
+                if (this.isNeuronSeed(cellPair.getLeft())) {
+                    builder.state(CellState.NEURON);
+                    final Direction gate = this.getNeuronGate(); 
+                    neighborsPreviousInput = RulesUtils.newFilledEnumMap(Signal.DENDRITE_SIGNAL.getValue());
+                    neighborsPreviousInput.put(gate, Signal.AXON_SIGNAL.getValue());
+                    neighborsPreviousInput.put(gate.getOpposite(), Signal.AXON_SIGNAL.getValue());
+                    break;
+                }
+                int inputSum = RulesUtils.sumEnumMapValues(cell.getNeighborsPreviousInput());
+                if (inputSum == 0) {
+                    break;
+                }
+                inputSum = RulesUtils.sumEnumMapSpecificValues(neighborsPreviousInput, Signal.AXON_SIGNAL.getValue());
+                if (inputSum == Signal.AXON_SIGNAL.getValue()) {
+                    builder.state(CellState.AXON);
+                    final Direction direction = cell.getNeighborsPreviousInput().entrySet().stream().
+                        filter(e -> e.getValue() == Signal.AXON_SIGNAL.getValue()).
+                        map(e -> e.getKey()).
+                        findFirst().get();
+                    builder.gate(direction); 
+                    for (final var d: Direction.values()) {
+                        neighborsPreviousInput.put(d, cell.getChromosome().get(d) ? Signal.AXON_SIGNAL.getValue() : 0);
+                    }
+                    break;
+
+                }
+                if (inputSum > Signal.AXON_SIGNAL.getValue()) {
+                    neighborsPreviousInput = RulesUtils.newFilledEnumMap(0);
+                    break;
+                }
+                inputSum = RulesUtils.sumEnumMapSpecificValues(neighborsPreviousInput, Signal.DENDRITE_SIGNAL.getValue());
+                if (inputSum == Signal.DENDRITE_SIGNAL.getValue()) {
+                    builder.state(CellState.DENDRITE);
+                    final Direction direction = cell.getNeighborsPreviousInput().entrySet().stream().
+                            filter(e -> e.getValue() == Signal.DENDRITE_SIGNAL.getValue()).
+                            map(e -> e.getKey()).
+                            findFirst().get();
+                    builder.gate(direction.getOpposite());
+                    for (final var d: Direction.values()) {
+                        neighborsPreviousInput.put(d, cell.getChromosome().get(d) ? Signal.DENDRITE_SIGNAL.getValue() : 0);
+                    }
+                    break;
+                }
+                builder.state(cell.getState());
+                neighborsPreviousInput = RulesUtils.newFilledEnumMap(0);
+                break;
+            case NEURON:
+                neighborsPreviousInput = RulesUtils.newFilledEnumMap(Signal.DENDRITE_SIGNAL.getValue());
+                neighborsPreviousInput.put(cell.getGate().get(), Signal.AXON_SIGNAL.getValue());
+                neighborsPreviousInput.put(cell.getOppositeToGate().get(), Signal.AXON_SIGNAL.getValue());
+                break;
+            case AXON:
+                for (final var d: Direction.values()) {
+                    neighborsPreviousInput.put(d, cell.getChromosome().get(d) ? Signal.AXON_SIGNAL.getValue() : 0);
+                }
+                break;
+            case DENDRITE:
+                for (final var d: Direction.values()) {
+                    neighborsPreviousInput.put(d, cell.getChromosome().get(d) ? Signal.DENDRITE_SIGNAL.getValue() : 0);
+                }
+                break;
+            default:
+                break;
+        }
+        builder.activationCounter(cell.getActivationCounter());
+        builder.neighborsPreviousInput(neighborsPreviousInput);
+        return builder.build();
+    }
+
+    private Direction getNeuronGate() {
+        Direction direction = Direction.getRandomDirection();
+        while (direction == Direction.TOP || direction == Direction.BOTTOM) {
+            direction = Direction.getRandomDirection();
+        }
+        return direction;
+    }
+
+    private boolean isNeuronSeed(final Coordinates3D<Integer> coord) {
+        return RulesUtils.rand25() && RulesUtils.rand25() 
+                && coord.getY() % 2 + coord.getZ() % 2 == 0;
     }
 
 }
