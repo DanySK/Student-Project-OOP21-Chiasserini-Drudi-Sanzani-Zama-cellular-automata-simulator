38abf2dff22003685842f156c433a5f4ae5359fb
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-03-23 13:51:55 +0100

    `SignalingUpdateRule` refactor.

--- app/src/main/java/casim/model/codi/rule/SignalingUpdateRule.java
+++ app/src/main/java/casim/model/codi/rule/SignalingUpdateRule.java
@@ -11,6 +11,7 @@ import casim.model.codi.cell.CoDiCell;
 import casim.model.codi.cell.attributes.Direction;
 import casim.model.codi.cell.builder.CoDiCellBuilder;
 import casim.model.codi.cell.builder.CoDiCellBuilderImpl;
+import casim.model.codi.utils.CodiUtils;
 import casim.utils.coordinate.Coordinates3D;
 import casim.utils.grid.Grid;
 
@@ -36,38 +37,21 @@ public class SignalingUpdateRule extends AbstractUpdateRule<Coordinates3D<Intege
     @Override
     protected CoDiCell nextCell(final Pair<Coordinates3D<Integer>, CoDiCell> cellPair,
             final List<Pair<Coordinates3D<Integer>, CoDiCell>> neighborsPairs) {
-        int inputSum;
         final CoDiCell cell = cellPair.getRight();
-        final CoDiCellBuilder builder = new CoDiCellBuilderImpl();
-        EnumMap<Direction, Integer> neighborsPreviousInput = RulesUtils.newFilledEnumMap(0);
+        CoDiCellBuilder builder = new CoDiCellBuilderImpl();
         switch (cell.getState()) {
             case BLANK:
                 builder.activationCounter(cell.getActivationCounter());
+                builder.neighborsPreviousInput(CodiUtils.newFilledEnumMap(() -> 0));
                 break;
             case NEURON:
-                inputSum = 1 + RulesUtils.sumEnumMapValues(cell.getNeighborsPreviousInput())
-                        - cell.getNeighborsPreviousInput().get(cell.getGate().get());
-                if (cell.getNeighborsPreviousInput().containsKey(cell.getOppositeToGate().get())) {
-                    inputSum -= cell.getNeighborsPreviousInput().get(cell.getOppositeToGate().get());
-                }
-                if (cell.getActivationCounter() + inputSum > NEURON_ACTIVATION_VALUE) {
-                    neighborsPreviousInput.put(cell.getGate().get(), 1);
-                    neighborsPreviousInput.put(cell.getOppositeToGate().get(), 1);
-                    builder.activationCounter(0);
-                } else {
-                    builder.activationCounter(cell.getActivationCounter() + inputSum);
-                }
+                builder = this.neuronSignal(cell);
                 break;
             case AXON:
-                neighborsPreviousInput = RulesUtils.newFilledEnumMap(cell.getActivationCounter());
-                builder.activationCounter((cell.getNeighborsPreviousInput().get(cell.getGate().get()) != 0) ? 1 : 0);
+                builder = this.axonSignal(cell);
                 break;
             case DENDRITE:
-                inputSum = RulesUtils.sumEnumMapValues(cell.getNeighborsPreviousInput());
-                inputSum = inputSum > 2 ? 2 : inputSum; 
-                neighborsPreviousInput = RulesUtils.newFilledEnumMap(0);
-                neighborsPreviousInput.put(cell.getGate().get(), inputSum);
-                builder.activationCounter((inputSum != 0) ? 1 : 0);
+                builder = this.dendriteSignal(cell);
                 break;
             default:
                 break;
@@ -75,8 +59,42 @@ public class SignalingUpdateRule extends AbstractUpdateRule<Coordinates3D<Intege
         return builder.state(cell.getState())
                       .gate(cell.getGate())
                       .chromosome(cell.getChromosome())
-                      .neighborsPreviousInput(neighborsPreviousInput)
                       .build();
+    }
+
+    private CoDiCellBuilder neuronSignal(final CoDiCell cell) {
+        final CoDiCellBuilder builder = new CoDiCellBuilderImpl();
+        final EnumMap<Direction, Integer> neighborsPreviousInput = CodiUtils.newFilledEnumMap(() -> 0);
+        int inputSum = 1 + CodiUtils.sumEnumMapValues(cell.getNeighborsPreviousInput())
+            - cell.getNeighborsPreviousInput().get(cell.getGate().get());
+        if (cell.getNeighborsPreviousInput().containsKey(cell.getOppositeToGate().get())) {
+            inputSum -= cell.getNeighborsPreviousInput().get(cell.getOppositeToGate().get());
+        }
+        if (cell.getActivationCounter() + inputSum > NEURON_ACTIVATION_VALUE) {
+            neighborsPreviousInput.put(cell.getGate().get(), 1);
+            neighborsPreviousInput.put(cell.getOppositeToGate().get(), 1);
+            builder.activationCounter(0);
+        } else {
+            builder.activationCounter(cell.getActivationCounter() + inputSum);
+        }
+        return builder.neighborsPreviousInput(neighborsPreviousInput);
+    }
+
+    private CoDiCellBuilder axonSignal(final CoDiCell cell) {
+        final CoDiCellBuilder builder = new CoDiCellBuilderImpl();
+        final EnumMap<Direction, Integer> neighborsPreviousInput = CodiUtils.newFilledEnumMap(() -> cell.getActivationCounter());
+        builder.activationCounter((cell.getNeighborsPreviousInput().get(cell.getGate().get()) != 0) ? 1 : 0);
+        return builder.neighborsPreviousInput(neighborsPreviousInput);
+    }
+
+    private CoDiCellBuilder dendriteSignal(final CoDiCell cell) {
+        final CoDiCellBuilder builder = new CoDiCellBuilderImpl();
+        final EnumMap<Direction, Integer> neighborsPreviousInput = CodiUtils.newFilledEnumMap(() -> 0);
+        int inputSum = CodiUtils.sumEnumMapValues(cell.getNeighborsPreviousInput());
+        inputSum = inputSum > 2 ? 2 : inputSum; 
+        neighborsPreviousInput.put(cell.getGate().get(), inputSum);
+        builder.activationCounter((inputSum != 0) ? 1 : 0);
+        return builder.neighborsPreviousInput(neighborsPreviousInput);
     }
 
 }
