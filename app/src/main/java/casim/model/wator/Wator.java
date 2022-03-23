370ca70c41414cca530fb3345c3b6854a324b63e
Simone Zama <simone.zama@studio.unibo.it>
2022-03-23 18:03:17 +0100

    Fix `Wator` implementation

--- app/src/main/java/casim/model/wator/Wator.java
+++ app/src/main/java/casim/model/wator/Wator.java
@@ -1,22 +1,29 @@
 package casim.model.wator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
 import org.apache.commons.lang3.tuple.Pair;
 import casim.model.abstraction.automaton.AbstractAutomaton;
 import casim.model.abstraction.utils.NeighborsFunctions;
 import casim.utils.PlayableAutomaton;
+import casim.utils.coordinate.Coordinates2D;
 import casim.utils.coordinate.CoordinatesUtil;
 import casim.utils.grid.Grid2D;
 import casim.utils.grid.Grid2DImpl;
 import casim.utils.range.Ranges;
 
 @PlayableAutomaton(AutomatonName = "Predators and Preys")
-public class Wator extends AbstractAutomaton<CellState, WatorCell>{
+public class Wator extends AbstractAutomaton<CellState, WatorCell> {
 
     private static final int MAX_HEALTH = 10;
     private static final int INIT_HEALTH = 5;
+    private static final int DEAD_HEALTH = 0;
 
     private Grid2D<WatorCell> state;
-    private final UpdateRule updateRule = new UpdateRule(NeighborsFunctions::neighbors2DFunction);
 
     public Wator(final Grid2D<CellState> state) {
         this.state = state.map(x -> new WatorCell(x, INIT_HEALTH, MAX_HEALTH));
@@ -30,14 +37,48 @@ public class Wator extends AbstractAutomaton<CellState, WatorCell>{
 
     @Override
     protected Grid2D<WatorCell> doStep() {
-        final var newState = new Grid2DImpl<WatorCell>(this.state.getHeight(), this.state.getWidth());
         for (final var x : Ranges.of(0, this.state.getHeight())) {
             for (final var y : Ranges.of(0, this.state.getWidth())) {
                 final var coord = CoordinatesUtil.of(x, y);
-                newState.set(coord, this.updateRule.getNextCell(Pair.of(coord, this.state.get(coord)), this.state));
+                final var currCell = this.state.get(coord);
+                final var neighborsList = CoordinatesUtil.get2DNeighbors(coord).stream()
+                        .filter(c -> this.state.isCoordValid(c))
+                        .map(c -> this.state.get(c))
+                        .collect(Collectors.toList());
+                final var preyNeighbors = neighborsList.stream()
+                        .filter(w -> w.getState().equals(CellState.PREY))
+                        .collect(Collectors.toList());
+                final var deadNeighbors = neighborsList.stream()
+                        .filter(w -> w.getState().equals(CellState.DEAD))
+                        .collect(Collectors.toList());
+                switch (currCell.getState()) {
+                    case PREY:
+                        if (deadNeighbors.size() > 0) {
+                            this.cellStep(currCell, deadNeighbors, WatorCell::heal);
+                        }
+                        else {
+                            currCell.heal();
+                            currCell.move();
+                        }
+                        break;
+                    case PREDATOR:
+                        if (preyNeighbors.size() > 0) {
+                            this.cellStep(currCell, preyNeighbors, WatorCell::heal);
+
+                        } else if (deadNeighbors.size() > 0) {
+                            this.cellStep(currCell, deadNeighbors, WatorCell::starve);
+                        } else {
+                            currCell.starve();
+                            currCell.move();
+                        }
+                        break;
+                    default:
+                        break;
+
+                }
             }
         }
-        this.state = newState;
+        this.state.stream().forEach(WatorCell::resetMovement);
         return this.state;
     }
 
@@ -45,5 +86,26 @@ public class Wator extends AbstractAutomaton<CellState, WatorCell>{
     public Grid2D<WatorCell> getGrid() {
         return this.state;
     }
-    
+
+    private void cellStep(final WatorCell currentCell, final List<WatorCell> neighbors, final Consumer<WatorCell> movementAction) {
+        if (currentCell.isDead()) {
+            currentCell.setState(CellState.DEAD);
+            currentCell.setHealth(DEAD_HEALTH);
+            currentCell.move();
+            return;
+        } else if (currentCell.hasMoved()) {
+            return;
+        }
+        final var rand = new Random();
+        final var toChange = neighbors.get(rand.nextInt(neighbors.size()));
+        toChange.setState(currentCell.getState());
+        toChange.setHealth(currentCell.getHealth());
+        movementAction.accept(toChange);
+        toChange.move();
+        final var spawn = toChange.reproduce();
+        currentCell.setState(spawn.getState());
+        currentCell.setHealth(spawn.getHealth());
+        currentCell.move();
+    }
+
 }
