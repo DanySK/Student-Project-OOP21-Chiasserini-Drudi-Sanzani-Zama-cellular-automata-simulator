95af2fa71630f97eff3bb6ae2888aad21d6a72ca
Simone Zama <simone.zama@studio.unibo.it>
2022-03-25 14:59:59 +0100

    Add javadoc comments

--- app/src/main/java/casim/model/langtonsant/CellState.java
+++ app/src/main/java/casim/model/langtonsant/CellState.java
@@ -1,6 +1,17 @@
 package casim.model.langtonsant;
 
+/**
+ * The state of a {@link casim.model.langtonsant.Ant}.
+ */
 public enum CellState {
+    /**
+     * The activated state, an ant on this state
+     * will turn left.
+     */
     ON,
+    /**
+     * The deactivated state, an ant on this state
+     * will turn right.
+     */
     OFF;
 }
