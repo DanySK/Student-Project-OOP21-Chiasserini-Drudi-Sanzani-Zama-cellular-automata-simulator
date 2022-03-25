f9d770755b0960a188fe6cfccf582540cc8ae5d9
Simone Zama <simone.zama@studio.unibo.it>
2022-03-25 13:39:49 +0100

    Change  to execute Predators and Preys

--- app/src/main/java/casim/App.java
+++ app/src/main/java/casim/App.java
@@ -3,17 +3,18 @@
  */
 package casim;
 
+import java.util.Random;
 import casim.controller.automaton.AutomatonController;
 import casim.controller.automaton.AutomatonControllerImpl;
-import casim.model.codi.CoDi;
-import casim.model.codi.cell.attributes.CellState;
+import casim.model.wator.CellState;
+import casim.model.wator.Wator;
 import casim.ui.components.grid.CanvasGridBuilderImpl;
 import casim.ui.components.grid.CanvasGridImpl;
 import casim.ui.components.page.PageContainer;
 import casim.ui.utils.StateColorMapper;
 import casim.ui.view.AutomatonView;
 import casim.utils.Colors;
-import casim.utils.grid.Grid3DImpl;
+import casim.utils.grid.Grid2DImpl;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -23,10 +24,9 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
-    private static final int ROWS = 20;
-    private static final int COLS = 20;
-    private static final int DEPTH = 20;
-    private static final int CELL_SIZE = 40;
+    private static final int ROWS = 600;
+    private static final int COLS = 600;
+    private static final int CELL_SIZE = 2;
 
     private CanvasGridImpl getGrid() {
         return (CanvasGridImpl) new CanvasGridBuilderImpl().build(ROWS, COLS, CELL_SIZE);
@@ -41,23 +41,26 @@ public class App extends Application {
      */
     @Override
     public void start(final Stage primaryStage) throws Exception {
-        final var state = new Grid3DImpl<CellState>(ROWS, COLS, DEPTH, () -> CellState.BLANK);
-        final var automaton = new CoDi(state);
+        final var state = new Grid2DImpl<CellState>(ROWS, COLS, () -> {
+            final var rand = new Random();
+            final var val = rand.nextInt(1_000);
+            if (val < 25) {
+                return CellState.PREY;
+            } else if (val < 100) {
+                return CellState.PREDATOR;
+            }
+            return CellState.DEAD;
+        });
+        final var automaton = new Wator(state);
         final var controller = new AutomatonControllerImpl<>(automaton);
         final var view = this.getView(primaryStage, controller, this.getGrid(), s -> {
             switch (s) {
-                case BLANK:
+                case PREY:
+                    return Colors.GREEN;
+                case PREDATOR:
+                    return Colors.RED;
+                case DEAD:
                     return Colors.BLACK;
-                case AXON:
-                    return Colors.FUSCIA;
-                case DENDRITE:
-                    return Colors.ARCTIC;
-                case ACTIVATED_AXON:
-                    return Colors.CARROT;
-                case ACTIVATED_DENDRITE:
-                    return Colors.PARAKEET;
-                case NEURON:
-                    return Colors.AMETHYST;
                 default:
                     throw new IllegalArgumentException("Invalid state.");
             }
