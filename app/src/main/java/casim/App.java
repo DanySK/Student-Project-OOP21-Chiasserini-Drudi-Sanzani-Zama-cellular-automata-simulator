d00952de82a07ae4c44067ad79574e4ea10f6280
Simone Zama <simone.zama@studio.unibo.it>
2022-03-25 14:19:00 +0100

    Change `App` to run `Langtonsant`

--- app/src/main/java/casim/App.java
+++ app/src/main/java/casim/App.java
@@ -5,15 +5,15 @@ package casim;
 
 import casim.controller.automaton.AutomatonController;
 import casim.controller.automaton.AutomatonControllerImpl;
-import casim.model.codi.CoDi;
-import casim.model.codi.cell.attributes.CellState;
+import casim.model.langtonsant.CellState;
+import casim.model.langtonsant.LangtonsAnt;
 import casim.ui.components.grid.CanvasGridBuilderImpl;
 import casim.ui.components.grid.CanvasGridImpl;
 import casim.ui.components.page.PageContainer;
 import casim.ui.utils.StateColorMapper;
 import casim.ui.view.AutomatonView;
 import casim.utils.Colors;
-import casim.utils.grid.Grid3DImpl;
+import casim.utils.grid.Grid2DImpl;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -23,10 +23,9 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
-    private static final int ROWS = 20;
-    private static final int COLS = 20;
-    private static final int DEPTH = 20;
-    private static final int CELL_SIZE = 40;
+    private static final int ROWS = 100;
+    private static final int COLS = 100;
+    private static final int CELL_SIZE = 10;
 
     private CanvasGridImpl getGrid() {
         return (CanvasGridImpl) new CanvasGridBuilderImpl().build(ROWS, COLS, CELL_SIZE);
@@ -41,23 +40,15 @@ public class App extends Application {
      */
     @Override
     public void start(final Stage primaryStage) throws Exception {
-        final var state = new Grid3DImpl<CellState>(ROWS, COLS, DEPTH, () -> CellState.BLANK);
-        final var automaton = new CoDi(state);
+        final var state = new Grid2DImpl<CellState>(ROWS, COLS, () -> CellState.OFF);
+        final var automaton = new LangtonsAnt(state, 50);
         final var controller = new AutomatonControllerImpl<>(automaton);
         final var view = this.getView(primaryStage, controller, this.getGrid(), s -> {
             switch (s) {
-                case BLANK:
+                case ON:
+                    return Colors.WHITE;
+                case OFF:
                     return Colors.BLACK;
-                case AXON:
-                    return Colors.FUSCIA;
-                case DENDRITE:
-                    return Colors.ARCTIC;
-                case ACTIVATED_AXON:
-                    return Colors.CARROT;
-                case ACTIVATED_DENDRITE:
-                    return Colors.PARAKEET;
-                case NEURON:
-                    return Colors.AMETHYST;
                 default:
                     throw new IllegalArgumentException("Invalid state.");
             }
