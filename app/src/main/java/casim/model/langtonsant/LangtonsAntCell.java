bdabb24e724748b369b9a96e17f3e35e7b55d398
Simone Zama <simone.zama@studio.unibo.it>
2022-03-25 14:53:46 +0100

    Add `javadoc` comments

--- app/src/main/java/casim/model/langtonsant/LangtonsAntCell.java
+++ app/src/main/java/casim/model/langtonsant/LangtonsAntCell.java
@@ -2,9 +2,18 @@ package casim.model.langtonsant;
 
 import casim.model.abstraction.cell.AbstractCell;
 
-public class LangtonsAntCell extends AbstractCell<CellState>{
+/**
+ * A Cell of the {@link casim.model.langtonsant.LangtonsAnt} automaton.
+ */
+public class LangtonsAntCell extends AbstractCell<CellState> {
 
-    public LangtonsAntCell(CellState state) {
+    /**
+     * Constructs a new {@link LangtonsAntCell} with a
+     * {@link CellState} from argument.
+     * 
+     * @param state the {@link CellState} of the {@link LantonsAntCell}.
+     */
+    public LangtonsAntCell(final CellState state) {
         super(state);
     }
 }
