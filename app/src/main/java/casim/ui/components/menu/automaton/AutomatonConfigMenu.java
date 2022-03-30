d439b802280f6a64c451ab3cb5928271e5ad7cd4
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-03-30 18:18:00 +0200

    Fix import.

--- app/src/main/java/casim/ui/components/menu/automaton/AutomatonConfigMenu.java
+++ app/src/main/java/casim/ui/components/menu/automaton/AutomatonConfigMenu.java
@@ -11,12 +11,10 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 
 /**
  * An abstract class for the {@link casim.model.abstraction.automaton.Automaton}.
