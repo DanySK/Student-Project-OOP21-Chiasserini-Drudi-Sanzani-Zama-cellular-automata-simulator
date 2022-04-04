e6bbcc2129fa5bd73978cb9580135f38fcca4ed3
Simone Zama <simone.zama@studio.unibo.it>
2022-04-04 16:22:54 +0200

    Implement `getWator` method

--- app/src/main/java/casim/utils/automaton/AutomatonFactoryImpl.java
+++ app/src/main/java/casim/utils/automaton/AutomatonFactoryImpl.java
@@ -1,5 +1,7 @@
 package casim.utils.automaton;
 
+import java.util.Random;
+
 import casim.model.bryansbrain.BryansBrain;
 import casim.model.codi.CoDi;
 import casim.utils.grid.Grid2DImpl;
@@ -7,6 +9,8 @@ import casim.utils.grid.Grid3DImpl;
 import casim.model.codi.cell.attributes.CellState;
 import casim.model.langtonsant.LangtonsAnt;
 import casim.model.langtonsant.LangtonsAntCellState;
+import casim.model.wator.Wator;
+import casim.model.wator.WatorCellState;
 
 /**
  * {@link AutomatonFactory} implementation.
@@ -29,6 +33,16 @@ public class AutomatonFactoryImpl implements AutomatonFactory {
     public LangtonsAnt getLangtonsAnt(final int cols, final int rows, final int antNumber, final boolean wrapping) {
         final var state = new Grid2DImpl<>(rows, cols, () -> LangtonsAntCellState.OFF);
         return new LangtonsAnt(state, antNumber, wrapping);
+    }
+
+    @Override
+    public Wator getWator(int cols, int rows) {
+        final var rng = new Random();
+        final var state = new Grid2DImpl<>(rows, cols, () -> {
+            final var val = rng.nextInt(WatorCellState.values().length);
+            return WatorCellState.values()[val];
+        });
+        return new Wator(state);
     }
 
 }
