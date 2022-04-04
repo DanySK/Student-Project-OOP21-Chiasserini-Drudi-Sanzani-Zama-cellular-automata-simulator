8d18ce8ff07ad7ac7cf5864e2d50bb4be934c78f
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-04-04 17:48:27 +0200

    Add `StateColorMapperFactor`.

---
+++ app/src/main/java/casim/ui/utils/StateColorMapperFactory.java
@@ -0,0 +1,42 @@
+package casim.ui.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import casim.model.codi.cell.attributes.CellState;
+import casim.utils.Colors;
+import javafx.scene.paint.Color;
+
+/**
+ * Factory for {@link StateColorMapper} configuration.
+ */
+public final class StateColorMapperFactory {
+
+    private StateColorMapperFactory() {
+    }
+
+    /**
+     * Return the {@link StateColorMapper} for CoDi automaton.
+     * 
+     * @return the {@link StateColorMapper} for CoDi automaton.
+     */
+    public static StateColorMapper<CellState> getCoDiStateColorMapper() {
+        return new StateColorMapper<CellState>() {
+            private final List<CellState> stateList = Arrays.asList(CellState.values());
+            private final List<Color> colorList = List.of(Colors.BLACK, Colors.AMETHYST, Colors.FUSCIA,
+                    Colors.CARROT, Colors.ARCTIC, Colors.PARAKEET);
+
+            @Override
+            public Color toColor(final CellState state) {
+                throwsIfNotPresent(stateList, state);
+                return colorList.get(stateList.indexOf(state));
+            }
+        };
+    }
+
+    private static <T> void throwsIfNotPresent(final List<T> list, final T value) {
+        if (!list.contains(value)) {
+            throw new IllegalArgumentException("Invalid State");
+        }
+    }
+}
