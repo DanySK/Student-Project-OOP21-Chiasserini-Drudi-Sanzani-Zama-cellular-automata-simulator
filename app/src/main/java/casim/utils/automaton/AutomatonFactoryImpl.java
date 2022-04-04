b982b973f3cb614baaa2dcd318774509e2d641c0
Simone Zama <simone.zama@studio.unibo.it>
2022-04-04 16:01:58 +0200

    Implement `getLangtonsAnt` method

--- app/src/main/java/casim/utils/automaton/AutomatonFactoryImpl.java
+++ app/src/main/java/casim/utils/automaton/AutomatonFactoryImpl.java
@@ -2,8 +2,11 @@ package casim.utils.automaton;
 
 import casim.model.bryansbrain.BryansBrain;
 import casim.model.codi.CoDi;
+import casim.utils.grid.Grid2DImpl;
 import casim.utils.grid.Grid3DImpl;
 import casim.model.codi.cell.attributes.CellState;
+import casim.model.langtonsant.LangtonsAnt;
+import casim.model.langtonsant.LangtonsAntCellState;
 
 /**
  * {@link AutomatonFactory} implementation.
@@ -20,6 +23,12 @@ public class AutomatonFactoryImpl implements AutomatonFactory {
     public CoDi getCoDi(final int cols, final int rows, final int depth) {
         final var state = new Grid3DImpl<CellState>(cols, rows, depth, () -> CellState.BLANK);
         return new CoDi(state);
+    }
+
+    @Override
+    public LangtonsAnt getLangtonsAnt(final int cols, final int rows, final int antNumber, final boolean wrapping) {
+        final var state = new Grid2DImpl<>(rows, cols, () -> LangtonsAntCellState.OFF);
+        return new LangtonsAnt(state, antNumber, wrapping);
     }
 
 }
