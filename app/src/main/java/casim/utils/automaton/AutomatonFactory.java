ba435a05d0132ccba8208d7de0d8bc9f044c5f42
Simone Zama <simone.zama@studio.unibo.it>
2022-04-04 16:01:43 +0200

    Add `getLangtonsAnt` method

--- app/src/main/java/casim/utils/automaton/AutomatonFactory.java
+++ app/src/main/java/casim/utils/automaton/AutomatonFactory.java
@@ -2,6 +2,7 @@ package casim.utils.automaton;
 
 import casim.model.bryansbrain.BryansBrain;
 import casim.model.codi.CoDi;
+import casim.model.langtonsant.LangtonsAnt;
 
 /**
  * A factory for the automaton creation.
@@ -26,4 +27,20 @@ public interface AutomatonFactory {
      * @return a new {@link CoDi} automaton.
      */
     CoDi getCoDi(int cols, int rows, int depth);
+
+    /**
+     * Returns a new {@link LangtonsAnt} automaton.
+     * 
+     * @param cols the columns of the automaton grid.
+     * @param rows the rows of the automaton grid.
+     * @param antNumber the number of ants that will
+     *          randomly populate the grid.
+     * @param wrapping a boolean indicating if the
+     *          grid will be wrapping or not.
+     *          If true the ants won't die when reaching
+     *          the edge of the grid but will warp to the
+     *          opposite edge.
+     * @return a new {@link LangtonsAnt} automaton.
+     */
+    LangtonsAnt getLangtonsAnt(int cols, int rows, int antNumber, boolean wrapping);
 }
