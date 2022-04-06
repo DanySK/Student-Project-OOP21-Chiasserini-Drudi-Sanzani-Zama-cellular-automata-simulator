e3ba4b4f13a5b15c542fad6bc85472930a462ca4
Simone Zama <simone.zama@studio.unibo.it>
2022-04-06 17:11:11 +0200

    Implement `WrappingConfig`

---
+++ app/src/main/java/casim/utils/config/WrappingConfig.java
@@ -0,0 +1,33 @@
+package casim.utils.config;
+
+/**
+ * Configuration class that mant a boolean
+ * wrapping value indicating if the grid used
+ * is a wrapping grid or otherwise.
+ */
+public class WrappingConfig extends BaseConfig{
+
+    private final boolean wrapping;
+
+    /**
+     * Constructs a {@link WrappingConfig} with given values.
+     * 
+     * @param rows the number of rows of the grid.
+     * @param cols the number of columns of the grid.
+     * @param wrapping true if the grid is a wrapping grid.
+     */
+    public WrappingConfig(final int rows, final int cols, final boolean wrapping) {
+        super(rows, cols);
+        this.wrapping = wrapping;
+    }
+    
+    /**
+     * Retuns the wrapping value of the config.
+     * 
+     * @return true if the grid is a wrapping grid
+     *          false otherwise.
+     */
+    public boolean isWrapped() {
+        return this.wrapping;
+    }
+}
