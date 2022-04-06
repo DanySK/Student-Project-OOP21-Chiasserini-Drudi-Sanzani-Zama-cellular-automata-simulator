d378f37e27887e6c693db0bad1ee5d03b5c020de
Simone Zama <simone.zama@studio.unibo.it>
2022-04-06 17:14:40 +0200

    `LangtonsAntConfig` extends `WrappingConfig`

--- app/src/main/java/casim/model/langtonsant/LangtonsAntConfig.java
+++ app/src/main/java/casim/model/langtonsant/LangtonsAntConfig.java
@@ -1,14 +1,13 @@
 package casim.model.langtonsant;
 
+import casim.utils.config.WrappingConfig;
+
 /**
  * {@link casim.model.langtonsant.LangtonsAnt}
  * configuration class.
  */
-public class LangtonsAntConfig {
+public class LangtonsAntConfig extends WrappingConfig{
 
-    private final int rows;
-    private final int cols;
-    private final boolean wrapping;
     private final int antNumber;
 
     /**
@@ -21,38 +20,8 @@ public class LangtonsAntConfig {
      * @param antNumber the number of ants in the automaton.
      */
     public LangtonsAntConfig(final int rows, final int cols, final boolean wrapping, final int antNumber) {
-        this.rows = rows;
-        this.cols = cols;
-        this.wrapping = wrapping;
+        super(rows, cols, wrapping);
         this.antNumber = antNumber;
-    }
-
-    /**
-     * Returns the number of rows.
-     * 
-     * @return the number of rows of the grid.
-     */
-    public int getRows() {
-        return this.rows;
-    }
-
-    /**
-     * Returns the number of columns.
-     * 
-     * @return the number of columns of the grid.
-     */
-    public int getCols() {
-        return this.cols;
-    }
-
-    /**
-     * Returns the wrapping value.
-     * 
-     * @return true if the grid is a wrapping
-     *          grid, false otherwise.
-     */
-    public boolean isWrapped() {
-        return this.wrapping;
     }
 
     /**
