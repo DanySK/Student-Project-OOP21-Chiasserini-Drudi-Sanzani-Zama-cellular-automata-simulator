2ce3a437ee1ed99f8c1b632201214d429e8f1f9e
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-04-11 18:52:19 +0200

    Fix import.

--- app/src/main/java/casim/ui/components/menu/automaton/config/AutomatonConfigController.java
+++ app/src/main/java/casim/ui/components/menu/automaton/config/AutomatonConfigController.java
@@ -11,7 +11,6 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
